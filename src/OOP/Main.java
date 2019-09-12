1ee85e06fee800d0dcc90a9d85aaf6fd8c08f8e1
Artyom <divinefridaay@yandex.ru>
2019-09-12 21:33:40 +0300

    test commit

---
+++ src/OOP/Main.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args){
+       Integer a = 1;
+       Integer b = 1;
+        System.out.println(a.equals(b));
+    }
+}
---
+++ src/OOP/Student.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Student extends Person {
+
+    private String major;
+
+    public Student(String name, String major) {
+        super(name);
+        this.major = major;
+    }
+
+    @Override
+    public String getDescription() {
+        return "а student majoring in " + major;
+    }
+}
